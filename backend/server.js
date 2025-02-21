9a8e78c464957688cc2184c839832b57b5a5fce3
Veronika <151367278+VeronikaRiabysheva@users.noreply.github.com>
2025-02-21 11:20:38 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({limit: "10mb"})); // to parse the bodyof the request//  10мб для фоток
 app.use(cookieParser());
 
@@ -23,6 +25,15 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
